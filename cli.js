7238f21f606e9ecc0748f2f71056821daceaf37d
Jason Smith (work) <jason.h.smith@gmail.com>
2013-07-07 03:01:02 +0000

    Refactor the command-line tool to make room for a replication auditer

--- cli.js
+++ cli.js
@@ -1,56 +1,78 @@
 #!/usr/bin/env node
-// The audit_couchdb command-line interface.
 //
+// The audit_couchdb command-line interface.
 
 var fs = require('fs')
-  , lib = require('./lib')
-  , assert = require('assert')
-  , optimist = require('optimist')
-  , Audit = require('./audit_couchdb').CouchAudit
-  ;
+var assert = require('assert')
+var optimist = require('optimist')
+var audit_couchdb = require('./audit_couchdb')
 
-function usage() {
-  console.log([ 'usage: audit_couchdb <URL>'
-              , ''
-              ].join("\n"));
-}
 
-var argv = optimist.default({log: 'info'})
-                   .argv;
+var OPTS = optimist.usage('$0 <URL>')
+                   .describe('db=<db>', 'Only audit database <db>')
+                   .describe('log', 'Log level')
+                   .default('log', 'info')
+                   .describe('replication', 'Audit replications')
+                   .boolean('replication')
 
-var couch_url = argv._[0];
-if(!couch_url || argv.help) {
-  usage();
-  process.exit(couch_url ? 0 : 1);
-}
+function main() {
+  if(OPTS.argv.help)
+    return console.log(OPTS.help())
 
-if(!/^https?:\/\//.test(couch_url))
-  couch_url = 'http://' + couch_url;
+  var couch_url = OPTS.argv._[0]
+  if(!couch_url)
+    return console.error(OPTS.help())
 
-var couch = new Audit;
-couch.url = couch_url;
-couch.log.setLevel(argv.log);
-couch.only_dbs = (argv.db ? [argv.db] : null);
+  if(! couch_url.match(/^https?:\/\//))
+    couch_url = 'http://' + couch_url
 
-var count = 0;
-couch.on('vulnerability', function(problem) {
-  count += 1;
-  var msg = [count, problem.level, problem.fact].join("\t");
-  if(problem.hint)
-    msg += " | " + problem.hint;
+  var couch = OPTS.argv.replication
+                ? new audit_couchdb.Replication
+                : new audit_couchdb.CouchAudit
 
-  if(problem.level === 'low')
-    couch.log.info(msg);
-  else if(problem.level === 'medium')
-    couch.log.warn(msg);
-  else if(problem.level === 'high')
-    couch.log.error(msg);
+  couch.url = couch_url
+  couch.log.setLevel(OPTS.argv.log)
+
+  if(OPTS.argv.replication)
+    replication(couch)
   else
-    throw new Error("Unknown problem level: " + JSON.stringify(problem));
-})
+    security(couch)
 
-couch.on('end', function() {
-  couch.log.info("Scan complete");
-})
+  couch.start()
+}
 
-couch.start();
+function security(couch) {
+  couch.only_dbs = (OPTS.argv.db ? [OPTS.argv.db] : null)
+
+  var count = 0;
+  couch.on('vulnerability', function(problem) {
+    count += 1;
+    var msg = [count, problem.level, problem.fact].join("\t");
+    if(problem.hint)
+      msg += " | " + problem.hint;
+
+    if(problem.level === 'low')
+      couch.log.info(msg);
+    else if(problem.level === 'medium')
+      couch.log.warn(msg);
+    else if(problem.level === 'high')
+      couch.log.error(msg);
+    else
+      throw new Error("Unknown problem level: " + JSON.stringify(problem));
+  })
+
+  couch.on('end', function() {
+    couch.log.info("Scan complete");
+  })
+}
+
+function replication(couch) {
+  console.log('Audit replication')
+
+  couch.on('end', function() {
+    throw new Error('Replication audit ended')
+  })
+}
+
+if(require.main === module)
+  main()
